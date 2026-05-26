9396769216ae473c05b02d9d1c4086f8e3ed7bc0
Marsita <>
2026-05-26 12:52:39 +0200

    weak memes

--- supabase-config.js
+++ supabase-config.js
@@ -1,7 +1,7 @@
 window.SUPABASE_CONFIG = window.SUPABASE_CONFIG || {
-  supabaseUrl: "",
-  supabaseAnonKey: "",
+  supabaseUrl: "https://irqggstugsmfdqfphiuy.supabase.co",
+  supabaseAnonKey: "sb_publishable_6RHjusIOLhDG52eC1IHCkQ_RIhWetf5",
   ratingsTable: "image_ratings",
-  turnstileSiteKey: "",
-  requireCaptchaForAuth: false
+  turnstileSiteKey: "0x4AAAAAADWF9Wxn-7-ikgoN",
+  requireCaptchaForAuth: true
 };
